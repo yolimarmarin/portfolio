a279c5b678cfe5ada4678a6941c05d50ee5daf56
Yolimar Marin <yolimar.ma83@gmail.com>
2020-08-09 11:54:47 -0500

    refactor navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,48 +1,69 @@
 import style from "../../styles/Navbar.module.css";
-import Link from "next/link";
 import { useState } from "react";
 import * as Icon from "react-feather";
-
-const NavBarMobile = () => {
-  const [displayMenu, setDisplayMenu] = useState(false);
-
-  const onClickShowMenu = () => {
-    setDisplayMenu(!displayMenu);
-  };
-
-  return (
-      displayMenu ? (
-        <div className={style.containerMobile}>
-          <div className={style.containerShowMenu}>
-          <Icon.XCircle onClick={onClickShowMenu} />
-          </div>
-          <img
-            src="/profile.jpg"
-            className={style.navPhoto}
-            alt="Yolimar Marin"
-          />
-          <div className={style.options}>
-            <Link href="/">
-              <a className="routingButton">Home</a>
-            </Link>
-            <Link href="/about">
-              <a className="routingButton">About Me</a>
-            </Link>
-            <Link href="/portfolio">
-              <a className="routingButton">Portfolio</a>
-            </Link>
-            <Link href="/skills">
-              <a className="routingButton">Skills</a>
-            </Link>
-          </div>
-        </div>
-      ) : <div className={style.containerShowMenu}>
-        <Icon.Menu onClick={onClickShowMenu} />
-      </div>
-  );
-};
+import { useRouter } from "next/router";
+import { RoutingButton } from "../buttons";
 
 const NavBar = () => {
+  const router = useRouter();
+
+  const buildOptions = () =>
+    [
+      {
+        title: "Home",
+        onClick: () => router.push("/"),
+        active: router.pathname === "/",
+      },
+      {
+        title: "About Me",
+        onClick: () => router.push("/about"),
+        active: router.pathname === "/about",
+      },
+      {
+        title: "Portfolio",
+        onClick: () => router.push("/portfolio"),
+        active: router.pathname === "/portfolio",
+      },
+      {
+        title: "Skills",
+        onClick: () => router.push("/skills"),
+        active: router.pathname === "/skills",
+      },
+    ].map((option, key) => (
+      <RoutingButton
+        key={key}
+        title={option.title}
+        onClick={option.onClick}
+        active={option.active}
+      />
+    ));
+
+  const NavBarMobile = () => {
+    const [displayMenu, setDisplayMenu] = useState(false);
+
+    const onClickShowMenu = () => {
+      setDisplayMenu(!displayMenu);
+    };
+
+    return displayMenu ? (
+      <div className={style.containerMobile}>
+        <div className={style.containerShowMenu}>
+          <Icon.XCircle onClick={onClickShowMenu} />
+        </div>
+        <img
+          src="/profile.jpg"
+          className={style.navPhoto}
+          alt="Yolimar Marin"
+        />
+        <div className={style.options}>{buildOptions()}</div>
+      </div>
+    ) : (
+      <div className={style.containerShowMenu}>
+        <Icon.Menu onClick={onClickShowMenu} />
+      </div>
+    );
+  };
+
   return (
     <>
       <div className={style.container}>
@@ -51,20 +72,7 @@ const NavBar = () => {
           className={style.navPhoto}
           alt="Yolimar Marin"
         />
-        <div className={style.options}>
-          <Link href="/">
-            <a className="routingButton">Home</a>
-          </Link>
-          <Link href="/about">
-            <a className="routingButton">About Me</a>
-          </Link>
-          <Link href="/portfolio">
-            <a className="routingButton">Portfolio</a>
-          </Link>
-          <Link href="/skills">
-            <a className="routingButton">Skills</a>
-          </Link>
-        </div>
+        <div className={style.options}>{buildOptions()}</div>
       </div>
       <NavBarMobile />
     </>
